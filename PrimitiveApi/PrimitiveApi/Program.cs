154e225ff7afb1e1ed2fe48427e2ec079f214b16
okhara <otar.kharaishvili@gmail.com>
2021-01-25 20:10:13 +0400

    initial steps

---
+++ PrimitiveApi/PrimitiveApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimitiveApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello There!");
+        }
+    }
+}
